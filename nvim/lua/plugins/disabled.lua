8e09e8470b1270f701fb40b9320192f64e945b5a
haruixu <xulr0820@hotmail.com>
2024-02-14 20:04:18 +0100

    Formatting

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
-  { "rcarriga/nvim-notify",                     enabled = false },
-  { "folke/noice.nvim",                         enabled = false },
-  { "junegunn/fzf.vim",                         enabled = false },
-  { "nvim-telescope/telescope-fzf-native.nvim", enabled = false },
-  { "nvim-pack/nvim-spectre",                   enabled = false },
+    { "rcarriga/nvim-notify",                     enabled = false },
+    { "folke/noice.nvim",                         enabled = false },
+    { "junegunn/fzf.vim",                         enabled = false },
+    { "nvim-telescope/telescope-fzf-native.nvim", enabled = false },
+    { "nvim-pack/nvim-spectre",                   enabled = false },
 }
