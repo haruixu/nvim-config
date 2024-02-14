5234f2b3949d2fd7b38e5706f8e1163bacddc36a
haruixu <xulr0820@hotmail.com>
2024-02-14 16:08:10 +0100

    Committing config

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,8 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    { "<leader>gs", false },
+    { "<leader>/",  false },
+    { "<leader>fs", "<cmd>Telescope live_grep<cr>", desc = "Grep string" },
+  },
+}
