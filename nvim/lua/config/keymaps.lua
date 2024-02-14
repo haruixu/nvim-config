862da119d2ddf20e1a49ac8f60574155e9870afb
haruixu <xulr0820@hotmail.com>
2024-02-14 18:26:39 +0100

    Added keymaps from primeage, but lack fighting one eyed kirby

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,5 +3,22 @@
 -- Add any additional keymaps here
 local map = vim.keymap.set
 
+--Move stuff
+map("v", "J", ":m '>+1<cr>gv=gv")
+map("v", "K", ":m '<-2<cr>gv=gv")
+
+--Allow cursor to stay in place when appending line below
+map("n", "J", "mzJ`z")
+--Centralize cursor when page jumping
 map("n", "<C-u>", "<C-u>zz")
 map("n", "<C-d>", "<C-d>zz")
+
+--Paste stuff
+map("x", "<leader>p", "\"_dP", { desc = "Paste, keep buffer" })
+map("n", "<leader>y", "\"+y", { desc = "Use clipboard" })
+map("v", "<leader>y", "\"+y", { desc = "Yank to clipboard" })
+--Same as "<leader>y", but don't have to press "y" twice
+map("n", "<leader>Y", "\"+Y", { desc = "Yank to clipboard" })
+
+map("n", "<leader>d", "\"_d", { desc = "Delete to void" })
+map("v", "<leader>d", "\"_d", { desc = "Delete to void" })
