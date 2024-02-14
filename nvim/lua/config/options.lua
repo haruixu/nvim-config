4203747c293882a7d84ecaf602cc33b5b0a361e0
haruixu <xulr0820@hotmail.com>
2024-02-14 17:32:04 +0100

    Added more options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,5 +1,19 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.scrolloff = 8
-vim.opt.hlsearch = false
+local opt = vim.opt
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+
+-- opt.swapfile = false
+-- opt.backup = false
+-- opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+-- opt.undofile = true
+
+opt.scrolloff = 8
+
+opt.updatetime = 50
+
+opt.hlsearch = false
