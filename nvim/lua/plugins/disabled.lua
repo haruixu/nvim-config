f1159b60cce219a4ac8cdfcf111e8b19423fb91d
haruixu <xulr0820@hotmail.com>
2024-02-17 14:08:59 +0100

    Switched neotree for oil

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -3,5 +3,5 @@ return {
     { "folke/noice.nvim",                         enabled = false },
     { "junegunn/fzf.vim",                         enabled = false },
     { "nvim-telescope/telescope-fzf-native.nvim", enabled = false },
-    { "nvim-pack/nvim-spectre",                   enabled = false },
+    { "nvim-neo-tree/neo-tree.nvim",              enabled = false },
 }
