652c912127cc7c523769214f894e61e431717305
haruixu <xulr0820@hotmail.com>
2024-02-17 14:21:09 +0100

    Added keymap to access file system with "-" in oil + added harpoon plugin

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,6 +1,8 @@
 return {
     'stevearc/oil.nvim',
-    opts = {},
+    opts = {
+        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    },
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
 }
