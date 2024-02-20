578f6cbb39e379a3783e355be9ba4ffaae1a5321
haruixu <xulr0820@hotmail.com>
2024-02-20 21:15:18 +0100

    Integrated system clipboard into nvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -15,6 +15,26 @@ opt.clipboard = "" --Overwrite clipboard sync (":set clipboard=" also worked)
 -- opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 -- opt.undofile = true
 
+local is_wsl = vim.fn.has("wsl") == 1
+
+-- WSL Clipboard support
+if is_wsl then
+  -- This is NeoVim's recommended way to solve clipboard sharing if you use WSL
+  -- See: https://github.com/neovim/neovim/wiki/FAQ#how-to-use-the-windows-clipboard-from-wsl
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
+
 opt.scrolloff = 8
 
 opt.updatetime = 50
